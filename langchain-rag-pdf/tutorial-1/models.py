a1aea2246c5184eb39e2e912093389e3fbfb7af6
PhilippKaem <kaemmerling.philipp2000@gmail.com>
2025-02-03 15:17:35 +0100

    OpenAI aus den Models entfernt
    OpenAI aus den Models entfernt

--- langchain-rag-pdf/tutorial-1/models.py
+++ langchain-rag-pdf/tutorial-1/models.py
@@ -1,6 +1,6 @@
 import os
 from langchain_ollama import OllamaEmbeddings, ChatOllama
-from langchain_openai import AzureOpenAIEmbeddings, AzureChatOpenAI
+# from langchain_openai import AzureOpenAIEmbeddings, AzureChatOpenAI
 
 class Models:
     def __init__(self):
@@ -14,22 +14,3 @@ class Models:
             model="llama3.2",
             temperature=0,
         )
-
-        # Azure OpenAI embeddings
-        self.embeddings_openai = AzureOpenAIEmbeddings(
-            model="text-embedding-3-large",
-            dimensions=1536,  # Can specify dimensions with new text-embedding-3 models
-            azure_endpoint=os.environ.get("AZURE_OPENAI_EMBEDDINGS_ENDPOINT"),
-            api_key=os.environ.get("AZURE_OPENAI_API_KEY"),
-            api_version=os.environ.get("AZURE_OPENAI_EMBEDDINGS_API_VERSION"),
-        )
-
-        # Azure OpenAI chat model
-        self.model_openai = AzureChatOpenAI(
-            azure_deployment=os.environ.get("AZURE_OPENAI_API_DEPLOYMENT_NAME"),
-            api_version=os.environ.get("AZURE_OPENAI_API_VERSION"),
-            temperature=0,
-            max_tokens=None,
-            timeout=None,
-            max_retries=2,
-        )  
