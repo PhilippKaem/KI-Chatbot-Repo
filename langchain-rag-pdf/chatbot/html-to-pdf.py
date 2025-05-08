2a4befac336759839f8db145db4792c75e674b31
PhilippKaem <kaemmerling.philipp2000@gmail.com>
2025-05-08 19:12:21 +0200

    Websitedaten
    Hinzufügen neuer Inhalte aus Websitedaten

--- langchain-rag-pdf/chatbot/html-to-pdf.py
+++ langchain-rag-pdf/chatbot/html-to-pdf.py
@@ -1,33 +1,29 @@
 import os
-from pyppeteer import launch
-import asyncio
+from playwright.sync_api import sync_playwright
 
-# Ein Python Skript, um HTML Seiten in PDF zu konvertieren
-# Input: Liste von Webseiten
-# Output: PDFs im Ordner 'Data'
-
-
+# Liste von Webseiten
 urls = [
-    'webseite-URL'
+    'https://www.ravensburg.dhbw.de/studienangebot/duale-studiengaenge-bachelor/duales-studium-data-science-und-kuenstliche-intelligenz',
+    'https://www.ravensburg.dhbw.de/studium-lehre/allgemeine-studienberatung#BeratungStudieninteressierte'
 ]
 
-# Pfad zum Speichern der PDFs
+# Ordner zum Speichern der PDFs
 output_dir = './data'
 os.makedirs(output_dir, exist_ok=True)
 
-# Konvertierung der URL zu PDF
-async def html_to_pdf(url, output_path):
-    browser = await launch()
-    page = await browser.newPage()
-    await page.goto(url, {'waitUntil': 'networkidle2'})
-    await page.pdf({'path': output_path, 'format': 'A4'})
-    await browser.close()
+# Funktion zur Umwandlung von URL zu PDF
+def html_to_pdf(url, output_path):
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        page.goto(url, wait_until="networkidle")
+        page.pdf(path=output_path, format="A4")
+        browser.close()
 
-
-# Konvertierung der URL zu PDF
+# Konvertierung jeder URL in ein PDF
 for i, url in enumerate(urls):
     output_path = os.path.join(output_dir, f'page_{i+1}.pdf')
-    print(f'Converting {url} to {output_path}')
-    asyncio.get_event_loop().run_until_complete(html_to_pdf(url, output_path))
+    print(f'Konvertiere {url} zu {output_path}')
+    html_to_pdf(url, output_path)
 
-print('Alle URLs in PDFs konvertiert')
+print('Alle URLs wurden erfolgreich in PDFs konvertiert.')
